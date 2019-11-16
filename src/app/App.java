71d5cf275116278eb383353090519e23f017f695
Gino Gustavo <ing.gino@gmail.com>
2019-11-16 12:28:33 -0600

    Adding Feature 3 class and removing that logic from main App class

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,4 @@
 package app;
-
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -12,9 +9,11 @@ public class App {
         App app = new App();
         app.diamondOperator();
         app.stringSwitch();
+        new Feature3_ResourceMgmt().autoClose();
+        
     }
 
-    /** Diamond operator */
+    /** Feature 1: Diamond operator */
     public void diamondOperator() {
 
         // Before
@@ -29,7 +28,7 @@ public class App {
 
     }
 
-    /** Using strings in switch statements */
+    /** Feature 2: Using strings in switch statements */
     public void stringSwitch() {
         String status = "Default";
 
@@ -59,42 +58,5 @@ public class App {
                 System.out.println("Process Default");
                 break;
         }
-    }
-
-    /**Automatic Resource Management */
-    public void autoClose() {
-        
-        //Before
-        FileInputStream fos1 = null;
-        DataOutputStream dos1 = null;
-        try {
-            fos1 = new FileInputStream("stocks.txt");
-            dos1 = new DataOutputStream(null);
-            dos1.writeUTF("testing writing files");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }finally{
-            try {
-                fos1.close();
-                dos1.close();
-            } catch (Exception e) {
-                //log exception
-            }
-        }
-
-        //--- Since Java 7
-
-        //try -> resource to be closed.
-        //Resources are enclosed in the try statement one after another separated by ;
-        //They are automatically closed, since behind classes inherit java.lang.AutoCloseable interface
-
-        try (FileInputStream fos = new FileInputStream("stocks.txt");
-            DataOutputStream dos = new DataOutputStream(null)) {
-                dos.writeUTF("Testing java 7 automatic resource management");
-        } catch (Exception e) {
-            // log exception
-        }
-
     }
 }
---
+++ src/app/Feature3_ResourceMgmt.java
@@ -0,0 +1,46 @@
+package app;
+
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+
+public class Feature3_ResourceMgmt {
+
+    /** Automatic Resource Management */
+    public void autoClose() {
+
+        // Before
+        FileInputStream fos1 = null;
+        DataOutputStream dos1 = null;
+        try {
+            fos1 = new FileInputStream("stocks.txt");
+            dos1 = new DataOutputStream(null);
+            dos1.writeUTF("testing writing files");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos1.close();
+                dos1.close();
+            } catch (Exception e) {
+                // log exception
+            }
+        }
+
+        // --- Since Java 7
+
+        // try -> resource to be closed.
+        // Resources are enclosed in the try statement one after another separated by ;
+        // They are automatically closed, since behind classes inherit
+        // java.lang.AutoCloseable interface
+
+        try (FileInputStream fos = new FileInputStream("stocks.txt");
+                DataOutputStream dos = new DataOutputStream(null)) {
+            dos.writeUTF("Testing java 7 automatic resource management");
+        } catch (Exception e) {
+            // log exception
+        }
+
+    }
+
+}
