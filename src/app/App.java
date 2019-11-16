cb541800027dd9f5dda4804d7043d09cf4508543
Gino Gustavo <ing.gino@gmail.com>
2019-11-16 11:38:05 -0600

    1st feature - Diamond operator

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,26 @@
 package app;
 
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Testing Java 7 features");
+    }
+
+    /** Diamond operator */
+    public void diamondOperator() {
+
+        // Before
+        Map<String, List<Integer>> stocks = new TreeMap<String, List<Integer>>();
+
+        // Since Java 7
+        Map<String, List<Integer>> newstocks = new TreeMap<>();
+
+        // Omiting the diamond operator (<>) is Legal,
+        // However, the compiler will generate couple of type-safety warnings
+        Map<String, List<Integer>> newstocks2 = new TreeMap();
+
     }
 }
