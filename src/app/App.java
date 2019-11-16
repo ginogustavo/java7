cef0bf6f34c73b46821d1593443beac6b16eb830
Gino Gustavo <ing.gino@gmail.com>
2019-11-16 11:49:35 -0600

    Adding String use in Switch

--- src/app/App.java
+++ src/app/App.java
@@ -23,4 +23,35 @@ public class App {
         Map<String, List<Integer>> newstocks2 = new TreeMap();
 
     }
+
+    public void stringSwitch() {
+        String status = "Default";
+
+        // Before
+        if(status.equalsIgnoreCase("ignore")){
+            System.out.println("Process Ignore");
+        }else if (status.equalsIgnoreCase("severe")) {
+            System.out.println("Process Severe");
+        }else if (status.equalsIgnoreCase("warning")) {
+            System.out.println("Process Warning");
+        }else{
+            System.out.println("Process Default");
+        }
+
+        //Since Java 7
+        switch(status){ // it use String.equals()
+            case "ignore":
+                System.out.println("Process Ignore");
+                break;
+            case "severe":
+                System.out.println("Process Severe");
+                break;
+            case "warning":
+                System.out.println("Process Warning");
+                break;
+            default:
+                System.out.println("Process Default");
+                break;
+        }
+    }
 }
