889de1b247361b7689c8ff6e216430c2e7550f67
Gino Gustavo <ing.gino@gmail.com>
2019-11-16 12:20:56 -0600

    Adding Automatic Resource Management

--- src/app/App.java
+++ src/app/App.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -7,6 +9,9 @@ import java.util.TreeMap;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Testing Java 7 features");
+        App app = new App();
+        app.diamondOperator();
+        app.stringSwitch();
     }
 
     /** Diamond operator */
@@ -24,6 +29,7 @@ public class App {
 
     }
 
+    /** Using strings in switch statements */
     public void stringSwitch() {
         String status = "Default";
 
@@ -53,5 +59,42 @@ public class App {
                 System.out.println("Process Default");
                 break;
         }
+    }
+
+    /**Automatic Resource Management */
+    public void autoClose() {
+        
+        //Before
+        FileInputStream fos1 = null;
+        DataOutputStream dos1 = null;
+        try {
+            fos1 = new FileInputStream("stocks.txt");
+            dos1 = new DataOutputStream(null);
+            dos1.writeUTF("testing writing files");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            try {
+                fos1.close();
+                dos1.close();
+            } catch (Exception e) {
+                //log exception
+            }
+        }
+
+        //--- Since Java 7
+
+        //try -> resource to be closed.
+        //Resources are enclosed in the try statement one after another separated by ;
+        //They are automatically closed, since behind classes inherit java.lang.AutoCloseable interface
+
+        try (FileInputStream fos = new FileInputStream("stocks.txt");
+            DataOutputStream dos = new DataOutputStream(null)) {
+                dos.writeUTF("Testing java 7 automatic resource management");
+        } catch (Exception e) {
+            // log exception
+        }
+
     }
 }
